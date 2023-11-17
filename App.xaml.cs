795524742ad5d66615fecf4393c30a178f146b34
PrzegrywXDLOL <142092638+PrzegrywXDLOL@users.noreply.github.com>
2023-11-17 16:56:28 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Turn_Based_Game
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Turn-Based-Game
+ The same game as console version but in windown and visible characters
